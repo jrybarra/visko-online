1ac658cb3c758ec49ecba09c2645e77bb5a13c4b
jrybarra <jrybarra@miners.utep.edu>
2014-05-01 19:11:56 -0600

    added bar graph to services

--- web/AnalyzeServices.php
+++ web/AnalyzeServices.php
@@ -1,9 +1,9 @@
 <?php
 	require_once("./include/membersite_config.php");
-	if(!$fgmembersite->CheckLogin()){
-		$fgmembersite->RedirectToURL("index.php");
-		exit;
-	}
+//	if(!$fgmembersite->CheckLogin()){
+//		$fgmembersite->RedirectToURL("index.php");
+//		exit;
+//	}
 	
 	if(isset($_POST['submitted'])){
 		/*if($fgmembersite->RegisterUser()){
@@ -11,7 +11,7 @@
 		}*/
 	}
 	
-	$nameOfPerson = $fgmembersite->UserEmail();
+//	$nameOfPerson = $fgmembersite->UserEmail();
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -56,18 +56,362 @@
 					- Most Supported Output Data Type: [[Type]]<br>
 					- Most Supported Service Toolkit: [[Toolkit Name]]<br><br><br>
 					
-					<img border="0" src="http://www.powertolearn.com/images/102606graph2.gif" alt="Pulpit rock" width="400px" style="padding-left:30px">
+					
 					<br><br><br>
 					
-					<p>	
-					Frequence of Service Errors
+					<p>
+						<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+						
+
+						<div id="container1" style="height: 400px; min-width: 310px;	max-width: 800px;margin: 0 auto;">
+							
+						<script>
+						
+							$(function () {
+							window.ViewerSets = getViewerSets();
+							window.Viewers = getViewers();
+							window.Tranformers = getTransformers();
+							window.Converters = getConverters();
+							window.Filters = getFilters();
+							window.Mappers = getMappers();
+							
+							 Highcharts.setOptions(Highcharts.theme);
+								$('#container1').highcharts({
+								chart: {
+									type: 'column',
+									margin: 75,
+									options3d: {
+										enabled: true,
+										alpha: 10,
+										beta: 25,
+										depth: 70
+									}
+								},
+								title: {
+									text: 'Frequency of Service Types'
+								},
+								subtitle: {
+									text: ''
+								},
+								plotOptions: {
+									column: {
+										depth: 25
+									}
+								},
+								xAxis: {
+									categories: ['Viewer Sets', 'Viewers', 'Transformers', 'Converters', 'Filters', 'Mappers']
+								},
+								yAxis: {
+									opposite: true
+								},
+								series: [{
+									name: 'Service Types',
+									data: [window.ViewerSets,window.Viewers,window.Tranformers, window.Converters, window.Filters, window.Mappers]
+								}]
+								});
+							});
+                            
+                            
+                            function getViewerSets(){
+                                return 101;
+                            }
+                            
+                            function getViewers(){
+                                return 44;
+                            }
+                            
+                            function getTransformers()
+                            {
+                                return 66;
+                            }
+                            function getConverters(){
+                                return 44;
+                            }
+                            function getFilters(){
+                                return 88;
+                            }
+                            function getMappers(){
+                                return 33;
+                            }
+						</script>    
+					</div>
+					</p>
+					<br><br><br><br><br><br>
+		
+					
+					
+					<p>
+					- Service with most associated errors: [[Service Name]]<br><br><br><br><br><br>
 					</p>
 					<p>
-					- Service with most associated errors: [[Service Name]]
+						<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+						<script src="http://code.highcharts.com/highcharts.js"></script>
+						<script src="http://code.highcharts.com/highcharts-3d.js"></script>
+						<script src="http://code.highcharts.com/modules/exporting.js"></script>
+
+						<div id="container" style="height: 400px; min-width: 310px;	max-width: 800px;margin: 0 auto;">
+							
+						<script>
+							
+							$(function () {
+							window.timeoutError = getTimeoutError();
+							window.inputDataError = getInputData();
+							window.serivceExecError= getServiceExec();
+							 Highcharts.setOptions(Highcharts.theme);
+								$('#container').highcharts({
+								chart: {
+									type: 'column',
+									margin: 75,
+									options3d: {
+										enabled: true,
+										alpha: 10,
+										beta: 25,
+										depth: 70
+									}
+								},
+								title: {
+									text: 'Frequency of Service Errors'
+								},
+								subtitle: {
+									text: ''
+								},
+								plotOptions: {
+									column: {
+										depth: 25
+									}
+								},
+								xAxis: {
+									categories: ['Service Timeout', 'Inupt Data', 'Service Execution']
+								},
+								yAxis: {
+									opposite: true
+								},
+								series: [{
+
+									name: 'Errors',
+									data: [window.timeoutError, window.inputDataError, window.serivceExecError]
+								}]
+								});
+							});
+							Highcharts.createElement('link', {
+   href: 'http://fonts.googleapis.com/css?family=Unica+One',
+   rel: 'stylesheet',
+   type: 'text/css'
+}, null, document.getElementsByTagName('head')[0]);
+
+Highcharts.theme = {
+   colors: ["#0F3E58","#2b908f", "#90ee7e", "#f45b5b", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
+      "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
+   chart: {
+      backgroundColor: {
+         linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+         stops: [
+            [0, '#DDDDDD'],
+            [1, '#aaaaaa']
+         ]
+      },
+      style: {
+         fontFamily: "'Lucida Sans Unicode', 'Arial', 'Helvetica', 'sans-serif'"
+      },
+      plotBorderColor: '#606063'
+   },
+   title: {
+      style: {
+         color: '#333333',
+         textTransform: 'uppercase',
+         fontSize: '20px'
+      }
+   },
+   subtitle: {
+      style: {
+         color: '##333333',
+         textTransform: 'uppercase'
+      }
+   },
+   xAxis: {
+      gridLineColor: '#707073',
+      labels: {
+         style: {
+            color: '#333333'
+         }
+      },
+      lineColor: '#707073',
+      minorGridLineColor: '#505053',
+      tickColor: '#707073',
+      title: {
+         style: {
+            color: '##333333'
+
+         }
+      }
+   },
+   yAxis: {
+      gridLineColor: '#707073',
+      labels: {
+         style: {
+            color: '#333333'
+         }
+      },
+      lineColor: '#707073',
+      minorGridLineColor: '#505053',
+      tickColor: '#707073',
+      tickWidth: 1,
+      title: {
+         style: {
+            color: '#333333'
+         }
+      }
+   },
+   tooltip: {
+      backgroundColor: 'rgba(0, 0, 0, 0.85)',
+      style: {
+         color: '#F0F0F0'
+      }
+   },
+   plotOptions: {
+      series: {
+         dataLabels: {
+            color: '#B0B0B3'
+         },
+         marker: {
+            lineColor: '#333333'
+         }
+      },
+      boxplot: {
+         fillColor: '#505053'
+      },
+      candlestick: {
+         lineColor: 'white'
+      },
+      errorbar: {
+         color: 'white'
+      }
+   },
+   legend: {
+      itemStyle: {
+         color: '#333333'
+      },
+      itemHoverStyle: {
+         color: '#FFF'
+      },
+      itemHiddenStyle: {
+         color: '#606063'
+      }
+   },
+   credits: {
+      style: {
+         color: '#666'
+      }
+   },
+   labels: {
+      style: {
+         color: '#707073'
+      }
+   },
+
+   drilldown: {
+      activeAxisLabelStyle: {
+         color: '#F0F0F3'
+      },
+      activeDataLabelStyle: {
+         color: '#F0F0F3'
+      }
+   },
+
+   navigation: {
+      buttonOptions: {
+         symbolStroke: '#DDDDDD',
+         theme: {
+            fill: '#505053'
+         }
+      }
+   },
+
+   // scroll charts
+   rangeSelector: {
+      buttonTheme: {
+         fill: '#505053',
+         stroke: '#000000',
+         style: {
+            color: '#CCC'
+         },
+         states: {
+            hover: {
+               fill: '#707073',
+               stroke: '#000000',
+               style: {
+                  color: 'white'
+               }
+            },
+            select: {
+               fill: '#000003',
+               stroke: '#000000',
+               style: {
+                  color: 'white'
+               }
+            }
+         }
+      },
+      inputBoxBorderColor: '#505053',
+      inputStyle: {
+         backgroundColor: '#333',
+         color: 'silver'
+      },
+      labelStyle: {
+         color: 'silver'
+      }
+   },
+
+   navigator: {
+      handles: {
+         backgroundColor: '#666',
+         borderColor: '#AAA'
+      },
+      outlineColor: '#CCC',
+      maskFill: 'rgba(255,255,255,0.1)',
+      series: {
+         color: '#7798BF',
+         lineColor: '#A6C7ED'
+      },
+      xAxis: {
+         gridLineColor: '#505053'
+      }
+   },
+
+   scrollbar: {
+      barBackgroundColor: '#808083',
+      barBorderColor: '#808083',
+      buttonArrowColor: '#CCC',
+      buttonBackgroundColor: '#606063',
+      buttonBorderColor: '#606063',
+      rifleColor: '#FFF',
+      trackBackgroundColor: '#404043',
+      trackBorderColor: '#404043'
+   },
+
+   // special colors for some of the
+   legendBackgroundColor: 'rgba(0, 0, 0, 0.5)',
+   background2: '#505053',
+   dataLabelsColor: '#B0B0B3',
+   textColor: '#C0C0C0',
+   contrastTextColor: '#F0F0F3',
+   maskColor: 'rgba(255,255,255,0.3)'
+};
+                            
+                            function getTimeoutError(){
+							
+                                return 44;
+                            }
+                            function  getInputData(){
+                                return 33;
+                            }
+                            function getServiceExec(){
+                                return 22;
+                            }
+                            
+						</script>    
+					</div>
 					</p>
-					<div id = "ServiceGraph"  style="padding-left:30px">
-						<img border="0" src="http://www.powertolearn.com/images/102606graph2.gif" alt="Pulpit rock" width="370px" style="padding-left:30px">
-					</div>	
 					<br><br>
 					
 				</div>
